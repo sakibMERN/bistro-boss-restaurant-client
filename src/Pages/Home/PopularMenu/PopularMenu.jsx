2e0f47c128375424cca4d26594841c19d0ab1a45
sakibMERN <sakib.sr57@gmail.com>
2025-01-03 13:15:32 +0600

    update our menu page

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,34 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import SectionTitle from './../../../components/SectionTitle/SectionTitle';
-import MenuItem from '../../Shared/MenuItem/MenuItem';
+import React, { useEffect, useState } from "react";
+import SectionTitle from "./../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
-    console.log(menu);
-    useEffect(()=> {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItem = data.filter(item => item.category === "popular");
-            setMenu(popularItem);
-        })
-    }, [])
-    return (
-        <section className='my-20'> 
-            <SectionTitle heading={'Form Our Menu'} subHeading={'Popular Menu'}>
-
-            </SectionTitle>
-            <div className='grid md:grid-cols-2 gap-10 mt-14'>
-                {
-                    menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
-                }
-            </div>
-            <div className='flex justify-center mt-5'>
-            <button className='btn btn-warning btn-outline border-0 border-b-4'>View Full Menu</button>
-            </div>
-            
-        </section>
-    );
+  const [menu, loading] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
+  // const [menu, setMenu] = useState([]);
+  // console.log(menu);
+  // useEffect(()=> {
+  //     fetch('menu.json')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //         const popularItem = data.filter(item => item.category === "popular");
+  //         setMenu(popularItem);
+  //     })
+  // }, [])
+  return (
+    <section className="my-20">
+      <SectionTitle
+        heading={"Form Our Menu"}
+        subHeading={"Popular Menu"}
+      ></SectionTitle>
+      <div className="grid md:grid-cols-2 gap-10 mt-14">
+        {popular.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <div className="flex justify-center mt-5">
+        <button className="btn btn-warning btn-outline border-0 border-b-4">
+          View Full Menu
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default PopularMenu;
--- src/Pages/Manu/Menu/Menu.jsx
+++ src/Pages/Manu/Menu/Menu.jsx
@@ -2,17 +2,52 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import CoverImg from '../../../assets/menu/banner3.jpg'
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg';
+import saladImg from '../../../assets/menu/salad-bg.jpg';
 import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
+
     return (
         <div className='pt-[76px]'>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover menuImg={CoverImg} title={'Our Menu'}></Cover>
-            <PopularMenu></PopularMenu>
-            <h2 className="text-5xl">Menu Page</h2>
+            {/* Today's offer section */}
+            <section className='my-20'>
+                <SectionTitle subHeading={"Don't Miss"} heading={"Today's Offer"}></SectionTitle>
+                <MenuCategory items={offered}></MenuCategory>
+            </section>
+            {/* Desert Section */}
+            <section className='my-20'>
+                {/* <SectionTitle subHeading={"Don't Miss"} heading={"Today's Offer"}></SectionTitle> */}
+                <MenuCategory items={offered} title={"Dessert"} coverImg={dessertImg}></MenuCategory>
+            </section>
+
+            {/* Pizza Section */}
+            <section className='my-20'>
+                {/* <SectionTitle subHeading={"Don't Miss"} heading={"Today's Offer"}></SectionTitle> */}
+                <MenuCategory items={pizza} title={"Pizza"} coverImg={pizzaImg}></MenuCategory>
+            </section>
+
+            {/* Salad Section */}
+            <section className='my-20'>
+                {/* <SectionTitle subHeading={"Don't Miss"} heading={"Today's Offer"}></SectionTitle> */}
+                <MenuCategory items={salad} title={"Salad"} coverImg={saladImg}></MenuCategory>
+            </section>
+            
+            
         </div>
     );
 };
